d39ec5726f1b854da8265c75e56545a05f0ae885
Belginbalakrishanan <belginbalakrishnan@gmail.com>
2024-02-15 12:36:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomingApp;
+Console.WriteLine("Welcome to Authentication app");
+
+SignIn obj = new SignIn();
+
+Console.WriteLine(obj.Authenticate("admin", "admin@123"));
+
